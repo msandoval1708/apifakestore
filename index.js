e18a4816f61c7f77f533130c4e0ce48222899471
Mauricio <msandoval.es@tecnologicoargos.edu.ec>
2024-03-06 22:07:30 -0500

    actualizado

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-console.log("Hola mundo")
+
 
 const URL = "https://fakestoreapi.com/products";
 
+
+let titulo = document.getElementById("titulo")
+let precio = document.getElementById("precio")
+
+console.log()
+
+let tabla = "<div>"
 function getData(){
 
     fetch (URL)
     .then( response => response.json())
-    .then( data => console.log("Datos de la API",data))
+    .then( data => {
+        console.log("Datos de la API",data)
+        for (let i = 0; i < data.length; i++) {
+            //console.log(data[i].title)
+            let bloquehtml = `<p>$(data[i].title)</p>`
+            console.log(bloquehtml)
+            tabla += bloquehtml;
+          }
+
+        tabla += "<div>";
+        producto.innerHTLM = tabla;
+} )
+
+    
 }
+
 
 getData();
